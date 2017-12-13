0c22b27a67e2f74723fde5acf35525f49919ef2d
b.ponomarenko <b.ponomarenko@tinkoff.ru>
2017-12-13 13:31:36 +0300

    add code highlighting

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,33 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          `gatsby-remark-responsive-iframe`,
+          'gatsby-remark-use-jsx',
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+            // Class prefix for <pre> tags containing syntax highlighting;
+            // defaults to 'language-' (eg <pre class="language-js">).
+            // If your site loads Prism into the browser at runtime,
+            // (eg for use with libraries like react-live),
+            // you may use this to prevent Prism from re-processing syntax.
+            // This is an uncommon use-case though;
+            // If you're unsure, it's best to use the default value.
+              classPrefix: "gatsby-code-",
+            }
+          }
+        ],
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `src`,
-        path: `${__dirname}/src/`,
+        name: `content`,
+        path: `${__dirname}/content/`,
       },
     }
   ],
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 import Header from '../components/organisms/Header/Header';
+import 'prismjs/themes/prism-okaidia.css';
 import '../assets/scss/index.scss';
 
 const TemplateWrapper = ({ children }) => (
